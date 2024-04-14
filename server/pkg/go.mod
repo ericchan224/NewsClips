0757a2a1e32c499a522866d110500c3ba64fa962
ericchan224 <chenxk_ericchan@163.com>
2024-04-14 15:17:11 +0800

    修改pkg module名称

--- server/pkg/go.mod
+++ server/pkg/go.mod
@@ -1,4 +1,4 @@
-module pkg
+module github.com/ericchan224/NewsClips/server/pkg
 
 go 1.20
 
---
+++ server/usersvr/service/user.go
@@ -0,0 +1,9 @@
+package service
+
+import (
+	"github.com/ericchan224/NewsClips/server/pkg/pb"
+)
+
+type UserService struct {
+	pb.UnimplementedUserServiceServer
+}
